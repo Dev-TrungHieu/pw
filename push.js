b1a4c66aba113aa124660659d8434394c7db44f1
truongtrunghieu <truongtrung.hieu0812@gmail.com>
2020-08-28 14:03:54 +0700

    update

--- push.js
+++ push.js
@@ -3,11 +3,7 @@ const PUBLIC_KEY =
   "BLT_cjs_Oifc5CKDemkNCRcPoVkaa5oClej_oYOrXFDYgocg70T205MnDIkMZhOUBGQ2-RMOFfuGfQ2-GNkNX0c";
 const PRIVATE_KEY = "S2kJVVSeIrrxaqu38h7mgLwBjGCxvxrGi-rMqKjuKvs";
 
-push.setVapidDetails(
-  "mailto: truongtrung.hieu0812@gmail.com",
-  PUBLIC_KEY,
-  PRIVATE_KEY
-);
+push.setVapidDetails("mailto: team@teamdibo.tk", PUBLIC_KEY, PRIVATE_KEY);
 
 let sub = {
   endpoint:
--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-self.addEventListener('push', function(e) {
+self.addEventListener("push", function (e) {
   var options = {
-    body: 'This notification was generated from a push!',
-    icon: 'https://img.icons8.com/bubbles/2x/google-logo.png',
+    body: "This notification was generated from a push!",
+    icon: "https://img.icons8.com/bubbles/2x/google-logo.png",
     vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
-      primaryKey: '2'
+      primaryKey: "2",
     },
     actions: [
-      {action: 'explore', title: 'Explore this new world',
-        icon: 'images/checkmark.png'},
-      {action: 'close', title: 'Close',
-        icon: 'images/xmark.png'},
-    ]
+      {
+        action: "explore",
+        title: "Explore this new world",
+        icon: "https://img.icons8.com/bubbles/2x/google-logo.png",
+      },
+      { action: "close", title: "Close", icon: "images/xmark.png" },
+    ],
   };
-  e.waitUntil(
-    self.registration.showNotification('Hello world!', options)
-  );
+  e.waitUntil(self.registration.showNotification("Hello world!", options));
 });
