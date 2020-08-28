f3b448c34c72b818157229fa16e06bfdfe5c43ee
truongtrunghieu <truongtrung.hieu0812@gmail.com>
2020-08-28 13:42:15 +0700

    update

--- push.js
+++ push.js
@@ -9,5 +9,14 @@ push.setVapidDetails(
   PRIVATE_KEY
 );
 
-let sub = {};
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/dcBVezZevhk:APA91bGqhcumGMSa5P7O38JdG5pxYvPsvgnKDGfF6ruUJbI32jM2WZqew3UZGv8CNboTUNr3PdYYWUuXBxF2hk7ghox4RqbGO1D5Am0m-WZO7lXKKZtytAC4vsU1TiQyxJ5uF0bhHBgT",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BJq_hwMLcl3CqvDw6FJc0Pm151wmyL8URuEZOTEekl6NMzPCxpAPO5SOPHsOZH6nIgBWf4bmKW9pQWnI5rgpkls",
+    auth: "kSq_D-FUh195jIgUS0iROQ",
+  },
+};
 push.sendNotification(sub, "text message");
